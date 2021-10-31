39f6610d9d643abf56a7cecf0b30a025d09988ab
andrescrd <c.andresricardo@gmail.com>
2021-10-31 00:20:44 -0300

    add pick winner

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,9 +3,10 @@ pragma solidity >=0.5.0 <0.9.0;
 
 contract Lottery {    
     uint public ethToPay = 0.01 ether;
+    uint public minNumberPlayers = 10;
+
     address payable[] public players;
     address public manager;
-
 
     constructor() {
         manager = msg.sender;
@@ -16,13 +17,40 @@ contract Lottery {
         players.push(payable(msg.sender));
     }
 
+    function transferManager(address newManager) external onlyManager {
+        manager = newManager;
+    }   
+
     function setEthToPay(uint _ethToPay) external onlyManager {
         require(_ethToPay > 0);
         ethToPay = _ethToPay;
     }
 
+    function setMinNumberPlayers(uint _minNumberPlayers) external onlyManager {
+        require(_minNumberPlayers > 0);
+        minNumberPlayers = _minNumberPlayers;
+    }
+
     function getBalance() public view onlyManager returns (uint) {    
         return address(this).balance;
+    }
+
+    function random() public view returns (uint) {
+        return uint(keccak256(abi.encodePacked(
+            block.difficulty, 
+            block.timestamp, 
+            players.length)));
+    }
+
+    function pickWinner() public onlyManager  {
+        require(players.length >= minNumberPlayers);
+        uint winnerIndex = random() % players.length;
+        players[winnerIndex].transfer(getBalance());
+        players = new address payable[](0);
+    }
+
+    function numberOfPlayers() public view returns (uint) {
+        return players.length;
     }
 
     modifier onlyManager {
