747f19e1508a1f3fdfd86919428e24050a177c3a
andrescrd <c.andresricardo@gmail.com>
2021-10-30 23:40:40 -0300

    init contract

---
+++ contracts/Lottery.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Lottery {
+    address payable[] public players;
+    address public manager;
+
+    constructor() {
+        manager = msg.sender;
+    }
+}
